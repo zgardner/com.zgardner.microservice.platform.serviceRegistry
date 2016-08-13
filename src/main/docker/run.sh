ee387228b8eade5d93a3ff9a2abcce8c3f8a4d62
Zach Gardner <z.gardner@hotmail.com>
2016-08-13 13:57:41 -0500

    Fixed the host port to be 8999 so we don't conflict with configuration service.

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,3 +1,3 @@
 docker pull 192.168.159.1:7000/microservice/serviceregistry:latest
 docker rm $(docker stop $(docker ps -a | grep '192.168.159.1:7000/microservice/serviceregistry' | awk '{print $1;}')) && 0
-docker run -d -p 8889:8889 192.168.159.1:7000/microservice/serviceregistry:latest
+docker run -d -p 8999:8889 192.168.159.1:7000/microservice/serviceregistry:latest
